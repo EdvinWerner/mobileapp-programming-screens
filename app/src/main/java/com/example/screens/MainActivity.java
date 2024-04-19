11cad101c3ec6f21f8dddb550a269905d341603e
Edde2508 <edvin.werner@live.se>
2024-04-19 12:31:52 +0200

    Added button button and intent to the main file

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.screens;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.my_button);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+                intent.putExtra("name", "Edvin");
+                intent.putExtra("number", 3);
+                startActivity(intent);
+                Log.d("==>","Clicked the button");
+            }
+        });
     }
 }
